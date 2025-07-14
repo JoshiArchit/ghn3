76c622e9c8e2941fb1da4988bbd25b647c71310e
Archit Joshi <joshi.archit.2904@gmail.com>
2025-07-14 18:18:35 -0500

    smaller models for CIFAR10 transfer

--- fine_tune.py
+++ fine_tune.py
@@ -21,12 +21,13 @@ args = init_config(mode='train_ghn', ckpt=args_raw.ckpt, debug=0)
 
 # --------------- 2. CIFAR-10 loader -------------------
 tf = torchvision.transforms.Compose([
-    torchvision.transforms.Resize(224),  # 👈 Enables all big models
+    torchvision.transforms.RandomCrop(32, padding=4),  # keep native resolution
     torchvision.transforms.RandomHorizontalFlip(),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize((0.491, 0.482, 0.447),
                                      (0.247, 0.243, 0.262)),
 ])
+
 
 trainset = torchvision.datasets.CIFAR10(root="~/data", train=True, download=True, transform=tf)
 trainloader = DataLoader(trainset, batch_size=128, shuffle=True, num_workers=4)
@@ -42,18 +43,12 @@ logger = Logger(args.epochs * len(trainloader))
 # --------------- 4. Valid torchvision models -------------------
 ARCH_POOL = [
     lambda: torchvision.models.resnet18(num_classes=10),
-    lambda: torchvision.models.resnet50(num_classes=10),
+    lambda: torchvision.models.resnet34(num_classes=10),
     lambda: torchvision.models.mobilenet_v2(num_classes=10),
-    lambda: torchvision.models.mobilenet_v3_large(num_classes=10),
-    lambda: torchvision.models.densenet121(num_classes=10),
-    lambda: torchvision.models.vgg11_bn(num_classes=10),
-    lambda: torchvision.models.convnext_tiny(num_classes=10),
+    lambda: torchvision.models.shufflenet_v2_x0_5(num_classes=10),
     lambda: torchvision.models.squeezenet1_0(num_classes=10),
-    lambda: torchvision.models.shufflenet_v2_x1_0(num_classes=10),
-    lambda: torchvision.models.efficientnet_b0(num_classes=10),
-    lambda: torchvision.models.inception_v3(num_classes=10, aux_logits=False)
+    lambda: torchvision.models.densenet121(num_classes=10),
 ]
-
 
 
 def random_templates(k, device):
